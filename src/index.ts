b12201e9d3dbfbc1d062be72486b5c72a371d90e
Ryan Bucshon <busheezy@gmail.com>
2018-02-02 19:18:21 -0600

    some refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'jsonfile';
 import { join } from 'path';
 import * as appRootPathLib from 'app-root-path';
-import { Client } from 'eris';
+import * as Eris from 'eris';
 import { URL } from 'url';
 import * as _ from 'lodash';
 import * as debug from 'debug';
@@ -9,13 +9,13 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
 const log = debug('discord');
-const steamIdLib = require('steamid');
+const steamIDLib = require('steamid');
 
 const appRootPath = appRootPathLib.toString();
 const configPath = join(appRootPath, 'config', 'discord.json');
 const config = readFileSync(configPath);
 
-const bot = new Client(config.token);
+const bot = new Eris.Client(config.token);
 const app = express();
 
 app.use(bodyParser.json());
@@ -28,87 +28,11 @@ app.post('/', (req, res) => {
 
   if (body.state === 'start') {
     const { cts, ts } = body;
-
-    const ctIds = JSON.parse(cts);
-    const tIds = JSON.parse(ts);
-
-    const guild = bot.guilds.find((guild) => {
-      return guild.id === config.guildId;
-    });
-
-    const mainVoice = guild.channels.find((channel) => {
-      return channel.id === config.mainChannelId;
-    }) as any;
-
-    const mainMembers = Array.from(mainVoice.voiceMembers);
-
-    _.each(mainMembers, async (m: any) => {
-      const [id, member] = m;
-      const profile = await member.user.getProfile();
-      const accounts = profile.connected_accounts;
-
-      const steam = _.find(accounts, {
-        type: 'steam',
-      });
-
-      if (steam) {
-        const sid = new steamIdLib(steam.id);
-        sid.instance = steamIdLib.Instance.DESKTOP;
-        const steamId = sid.getSteamID64();
-
-        if (ctIds.includes(steamId)) {
-          await member.edit({
-            channelID: config.ctChannelId,
-          }).catch((e: any) => {
-            console.error(e);
-          });
-        }
-
-        if (tIds.includes(steamId)) {
-          await member.edit({
-            channelID: config.tChannelId,
-          }).catch((e: any) => {
-            console.error(e);
-          });
-        }
-      }
-    });
-
+    startOfMatch(cts, ts);
   }
 
   if (body.state === 'end') {
-    const guild = bot.guilds.find((guild) => {
-      return guild.id === config.guildId;
-    });
-
-    const ctsVoice = guild.channels.find((channel) => {
-      return channel.id === config.ctChannelId;
-    }) as any;
-
-    const tsVoice = guild.channels.find((channel) => {
-      return channel.id === config.tChannelId;
-    }) as any;
-
-    const ctsMembers = Array.from(ctsVoice.voiceMembers);
-    const tsMembers = Array.from(tsVoice.voiceMembers);
-
-    _.each(ctsMembers, async (m: any) => {
-      const [id, member] = m;
-      await member.edit({
-        channelID: config.mainChannelId,
-      }).catch((e: any) => {
-        console.error(e);
-      });
-    });
-
-    _.each(tsMembers, async (m: any) => {
-      const [id, member] = m;
-      await member.edit({
-        channelID: config.mainChannelId,
-      }).catch((e: any) => {
-        console.error(e);
-      });
-    });
+    endOfMatch();
   }
 
   res.send('Success');
@@ -119,5 +43,80 @@ bot.on('ready', () => {
     console.log('listening on port 3535!');
   });
 });
+
+function startOfMatch(cts: string, ts: string) {
+
+  const ctIDs = JSON.parse(cts);
+  const tIDs = JSON.parse(ts);
+
+  const guild = bot.guilds.find((guild) => {
+    return guild.id === config.guildID;
+  });
+
+  const mainVoice = guild.channels.find((channel) => {
+    return channel.id === config.mainChannelID;
+  }) as any;
+
+  const mainMembers = Array.from(mainVoice.voiceMembers);
+
+  _.each(mainMembers, async (m: any) => {
+    const [id, member] = m;
+    const profile = await member.user.getProfile();
+    const accounts = profile.connected_accounts;
+
+    const steam = _.find(accounts, {
+      type: 'steam',
+    });
+
+    if (steam) {
+      const sid = new steamIDLib(steam.id);
+      sid.instance = steamIDLib.Instance.DESKTOP;
+      const steamID = sid.getSteamID64();
+
+      if (ctIDs.includes(steamID)) {
+        await moveMemberToChannel(member, config.ctChannelID);
+      }
+
+      if (tIDs.includes(steamID)) {
+        await moveMemberToChannel(member, config.tChannelID);
+      }
+    }
+  });
+}
+
+function endOfMatch() {
+  const guild = bot.guilds.find((guild) => {
+    return guild.id === config.guildID;
+  });
+
+  const ctsVoice = guild.channels.find((channel) => {
+    return channel.id === config.ctChannelID;
+  }) as any;
+
+  const tsVoice = guild.channels.find((channel) => {
+    return channel.id === config.tChannelID;
+  }) as any;
+
+  const ctsMembers = Array.from(ctsVoice.voiceMembers);
+  const tsMembers = Array.from(tsVoice.voiceMembers);
+
+  _.each(ctsMembers, async (m: any) => {
+    const [id, member] = m;
+    await moveMemberToChannel(member, config.mainChannelID);
+  });
+
+  _.each(tsMembers, async (m: any) => {
+    const [id, member] = m;
+    await moveMemberToChannel(member, config.mainChannelID);
+  });
+}
+
+async function moveMemberToChannel(member: any, channelID: string) {
+  await member.edit({
+    channelID,
+  }).catch((e: any) => {
+    console.error(e);
+  });
+}
 
 bot.connect();
